09c466d7a15ad5a3fe8d25b7c1a907d31263d4e3
Assassin010 <gauthier.kwatatshey@gmail.com>
2025-06-01 13:49:33 +0200

    fix: GENOPSX-8 adding atlantis repo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,41 @@
 module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+      parserOpts: {
+        // Match: type(scope)?: JIRA-TICKET subject
+        headerPattern: /^(\w+)(?:\(([^)]+)\))?!?: (GENOPSX|GAUT)-\d+ (.+)$/,
+        headerCorrespondence: ['type', 'scope', 'jira', 'subject'],
+      },
+    },
+    formatter: '@commitlint/format',
     rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'build',
+          'chore',
+          'ci',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'test',
+        ],
+      ],
+      'type-case': [2, 'always', 'lower-case'],
+      'type-empty': [2, 'never'],
+      'scope-case': [2, 'always', 'lower-case'],
+      'subject-empty': [2, 'never'],
+  
+      // ✅ Disable subject-case to allow ticket prefix
       'subject-case': [0],
+  
+      'body-leading-blank': [1, 'always'],
+      'footer-leading-blank': [1, 'always'],
     },
   };
   
